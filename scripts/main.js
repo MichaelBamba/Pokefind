35b7a7d1973774598984dbd30a039b7523b37936
MichaelBamba <mjbamba2@gmail.com>
2020-02-13 10:25:53 -0500

    removed redundancy

--- scripts/main.js
+++ scripts/main.js
@@ -2,53 +2,54 @@
 
 apiurl = `https://pokeapi.co/api/v2/pokemon/?limit=900`
 pokeListLocation = document.querySelector("#pokelist")
-let pokemon = "bulbasaur"
+
 let information = document.querySelector(".information")
 
-function getPokemon(){
+function getPokemon() {
     fetch(apiurl, {
-        "method": "GET"
+
     })
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        
-        let pokeList = data.results;
-        
-        pokeList.map(function(pokemon){
-            const pokeElement = document.createElement("option")
-            pokeElement.value = (pokemon.name)
-            pokeElement.textContent = (pokemon.name)
-            pokeListLocation.append(pokeElement) 
+        .then(response => {
+            return response.json()
         })
-        
-    })
+        .then(data => {
+
+            let pokeList = data.results;
+
+            pokeList.map(function (pokemon) {
+                const pokeElement = document.createElement("option")
+                pokeElement.value = (pokemon.name)
+                pokeElement.textContent = (pokemon.name)
+                pokeListLocation.append(pokeElement)
+            })
+
+        })
 };
 
-function display(){
+function display() {
 
     let pokemonSelected = document.querySelector("#pokelist").value
-    
+
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonSelected}`, {
         "method": "GET"
     })
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        
-        let abilities = data.abilities
-        abilities.map(function(abilities){
-            const pokeAbilities = document.createElement("p")
-            pokeAbilities.value = (abilities.ability.name)
-            pokeAbilities.textContent = (abilities.ability.name)
-            information.append(pokeAbilities) 
-            
-    })
-    })};
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
 
-document.getElementById("primarybtn").addEventListener("click", function(){
+            let abilities = data.abilities
+            abilities.map(function (abilities) {
+                const pokeAbilities = document.createElement("p")
+                pokeAbilities.value = (abilities.ability.name)
+                pokeAbilities.textContent = (abilities.ability.name)
+                information.append(pokeAbilities)
+
+            })
+        })
+};
+
+document.getElementById("primarybtn").addEventListener("click", function () {
     document.querySelector(".information").innerHTML = ""
     display()
 
