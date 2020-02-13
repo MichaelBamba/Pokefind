f6a86b7ff45b66d2892b06cf5a9c7265d783ed65
Frank Sundstedt <frank.sundstedt@gmail.com>
2020-02-13 15:40:03 -0500

    Commit changes including dictionary.

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,11 @@
 
 apiurl = `https://pokeapi.co/api/v2/pokemon/?limit=900`
 pokeListLocation = document.querySelector("#pokelist")
-let pokemon = "bulbasaur"
 let information = document.querySelector(".information")
+let comps = document.querySelector("#speedComp")
 
 function getPokemon(){
     fetch(apiurl, {
-        "method": "GET"
     })
     .then(response => {
         return response.json()
@@ -22,7 +21,6 @@ function getPokemon(){
             pokeElement.textContent = (pokemon.name)
             pokeListLocation.append(pokeElement) 
         })
-        
     })
 };
 
@@ -31,7 +29,6 @@ function display(){
     let pokemonSelected = document.querySelector("#pokelist").value
     
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonSelected}`, {
-        "method": "GET"
     })
     .then(response => {
         return response.json()
@@ -43,10 +40,37 @@ function display(){
             const pokeAbilities = document.createElement("p")
             pokeAbilities.value = (abilities.ability.name)
             pokeAbilities.textContent = (abilities.ability.name)
-            information.append(pokeAbilities) 
+            information.append(pokeAbilities)
+            })
+        let stats = data.stats
+        stats.map(stats => {
+            const pokeStat = document.createElement('li')
+            pokeStat.value = (stats.stat.name)
+            pokeStat.textContent = (stats.stat.name) + ": " + (stats.base_stat)
+            information.append(pokeStat)
             
     })
-    })};
+    })
+};
+
+function getPokemon(){
+    fetch(apiurl, {
+    })
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        
+        let pokeList = data.results;
+        
+        pokeList.map(function(pokemon){
+            const pokeElement = document.createElement("option")
+            pokeElement.value = (pokemon.name)
+            pokeElement.textContent = (pokemon.name)
+            pokeListLocation.append(pokeElement) 
+        })
+    })
+};
 
 document.getElementById("primarybtn").addEventListener("click", function(){
     document.querySelector(".information").innerHTML = ""
