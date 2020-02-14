1a7972bb11624d7a7daf55f6af706def55a4caea
Frank Sundstedt <frank.sundstedt@gmail.com>
2020-02-14 13:17:58 -0500

    added stats list

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,9 @@
 // User specific scripts go here
 
-<<<<<<< HEAD
-apiurl = `https://pokeapi.co/api/v2/pokemon/?limit=900`
-pokeListLocation = document.querySelector("#pokelist")
-let information = document.querySelector(".information")
-let comps = document.querySelector("#speedComp")
-=======
 const apiurl = `https://pokeapi.co/api/v2/pokemon/?limit=900`
 let information = document.querySelector(".information")
 let pokeSearchList = []
 let pokeSprites = document.querySelector('.pokeSprite')
->>>>>>> fdc1ef50b83d1660a35f431c622135eec49499cc
 
 function getPokemon() {
     fetch(apiurl, {
@@ -24,63 +17,18 @@ function getPokemon() {
                 pokeSearchList.push(pokemon.name)
             })
         })
-<<<<<<< HEAD
-    })
-=======
->>>>>>> fdc1ef50b83d1660a35f431c622135eec49499cc
 };
 
 function display() {
     let pokemonSelected = document.querySelector("#searchbar").value
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonSelected}`, {
     })
-<<<<<<< HEAD
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        
-        let abilities = data.abilities
-        abilities.map(function(abilities){
-            const pokeAbilities = document.createElement("p")
-            pokeAbilities.value = (abilities.ability.name)
-            pokeAbilities.textContent = (abilities.ability.name)
-            information.append(pokeAbilities)
-            })
-        let stats = data.stats
-        stats.map(stats => {
-            const pokeStat = document.createElement('li')
-            pokeStat.value = (stats.stat.name)
-            pokeStat.textContent = (stats.stat.name) + ": " + (stats.base_stat)
-            information.append(pokeStat)
-            
-    })
-    })
-};
-
-function getPokemon(){
-    fetch(apiurl, {
-    })
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        
-        let pokeList = data.results;
-        
-        pokeList.map(function(pokemon){
-            const pokeElement = document.createElement("option")
-            pokeElement.value = (pokemon.name)
-            pokeElement.textContent = (pokemon.name)
-            pokeListLocation.append(pokeElement) 
-        })
-    })
-=======
         .then(response => {
             return response.json()
         })
         .then(data => {
             let sprites = data.sprites.front_default
+
             console.log(sprites)
             let abilities = data.abilities
             abilities.map(function (abilities) {
@@ -88,6 +36,15 @@ function getPokemon(){
                 pokeAbilities.value = (abilities.ability.name)
                 pokeAbilities.textContent = (abilities.ability.name)
                 information.append(pokeAbilities)
+            })
+            
+            //creates stats list
+            let stats = data.stats
+            stats.map(stat => {
+                const pokeStat = document.createElement('li')
+                pokeStat.value = stat.stat.name
+                pokeStat.textContent = stat.stat.name + ": " + stat.base_stat
+                information.append(pokeStat)
             })
 
             // // document.createElement(img)
@@ -98,7 +55,6 @@ function getPokemon(){
             pokeSprites.append(pokeSprite)
             console.log(pokeSprite)
         })
->>>>>>> fdc1ef50b83d1660a35f431c622135eec49499cc
 };
 
 document.getElementById("primarybtn").addEventListener("click", function () {
